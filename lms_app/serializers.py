adaa06741b48ffe5d702816931047e14e812e18d
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-03 12:42:09 +0100

    fix category display

--- lms_app/serializers.py
+++ lms_app/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, Category
 
 class Book_serializer(serializers.ModelSerializer):
-	class Meta:
-		model = Book
-		fields = ('id', 'book_name', 'author', 'status', 'category', 'description',)
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['category'] = instance.category.category_name  # Access the name attribute
+        return representation
+
+    class Meta:
+        model = Book
+        fields = ('id', 'book_name', 'author', 'status', 'category', 'description',)
